c7c891835aaf40ccf0430dd2516d4c33441eba15
Andrew <androw1993@gmail.com>
2017-05-06 00:53:36 +0200

    Free and paid versions added

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
@@ -0,0 +1,15 @@
+package com.udacity.gradle.builditbigger.free;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.udacity.gradle.builditbigger.R;
+
+public class MainActivityFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+    }
+}
